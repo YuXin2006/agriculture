a0ff47a7fdd189dcd2a868eeaa85303195f72e36
YuXin2006 <2188279773@qq.com>
2026-05-26 00:31:57 +0800

    初始化表 连接上mysql数据库

--- backend/agri_backend/settings.py
+++ backend/agri_backend/settings.py
@@ -64,8 +64,16 @@ ASGI_APPLICATION = "agri_backend.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "agriculture",      # 你的数据库名
+        "USER": "root",       # 你的MySQL用户名
+        "PASSWORD": "12345678",   # 你的MySQL密码
+        "HOST": "localhost",           # MySQL服务器地址
+        "PORT": "3306",                # MySQL端口
+        "OPTIONS": {
+            "charset": "utf8mb4",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
